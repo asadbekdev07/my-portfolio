4bd532b4ea9c898306f218150baefcdcbb607bd8
asadbek07 <futureprotrader1007@gmail.com>
2024-07-19 23:49:38 +0500

    Update AOS animations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,20 +4,20 @@ const Contact = () => {
   return (
     <section id="contact" className="pt-20 pb-20 box-shadow-small">
       <div className="container-custom">
-        <h2 className="text-5xl text-center mb-10 text-white">Contact</h2>
+        <h2 className="text-5xl text-center mb-10 text-white" data-aos="fade-down">Contact</h2>
         <form className="w-[825px] m-auto">
           <div className="flex mb-5">
             <div className="relative mb-4 form-group">
-              <input className="form-input focus:outline-none" type="text" id="name" required />
+              <input className="form-input focus:outline-none" type="text" id="name" required data-aos="fade-right"/>
               <label className="form-label" htmlFor="name">Full Name</label>
             </div>
             <div className="relative mb-4 form-group">
-              <input className="form-input focus:outline-none" type="email" id="email" required />
+              <input className="form-input focus:outline-none" type="email" id="email" required data-aos="fade-left"/>
               <label className="form-label" htmlFor="email">Email</label>
             </div>
           </div>
           <div className="relative mb-4 form-group">
-            <textarea className="form-textarea focus:outline-none" id="textarea" rows="4" placeholder="Your message here — whether it’s about a potential project, a question, or just to say hello!" required></textarea>
+            <textarea className="form-textarea focus:outline-none" id="textarea" rows="4" placeholder="Your message here — whether it’s about a potential project, a question, or just to say hello!" required data-aos="fade-up"></textarea>
             <label className="form-label" htmlFor="textarea">Message</label>
           </div>
         </form>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import ReactTypingEffect from "react-typing-effect";
 const Hero = () => {
   return (
     <section className="container-custom text-white pt-[145px] pb-[190px] relative flex items-center justify-center">
-      <div className="w-[100%] text-center">
+      <div className="w-[100%] text-center flex flex-col items-center">
         <p className="text-5xl mb-5 text-shadow-custom" data-aos="fade-right">
           I'm{" "}
           <span className="text-[#ff9633] text-6xl text-shadow-custom-0">
@@ -17,29 +17,28 @@ const Hero = () => {
             />
           </span>
         </p>
-        <p className="text-5xl mb-5 text-shadow-custom" data-aos="fade-left">
-          Frontend Developer
-        </p>
-        <a href="#contact">
-          <button
-            className="button-custom mr-4 hover:box-shadow-big"
-            data-aos="fade-right"
-          >
-            Contact me
-          </button>
-        </a>
-        <a
-          href="/Resume.pdf"
-          download="Asadbek_Botirqulov_CV.pdf"
-          aria-label="Download CV"
-        >
-          <button
-            className="button-custom hover:box-shadow-big"
-            data-aos="fade-left"
-          >
-            Download CV <FontAwesomeIcon icon={faDownload} />
-          </button>
-        </a>
+        <p className="text-5xl mb-8 text-shadow-custom" data-aos="fade-left">
+  Frontend Developer
+</p>
+
+<div className="flex items-center justify-between w-[450px]">
+  <a 
+    href="#contact" 
+    className="button-custom hover:box-shadow-big" 
+    data-aos="fade-right"
+  >
+    Contact me
+  </a>
+  <a
+    className="button-custom hover:box-shadow-big"
+    href="/Resume.pdf"
+    download="Asadbek_Botirqulov_CV.pdf"
+    aria-label="Download CV"
+    data-aos="fade-left"
+  >
+    Download CV <FontAwesomeIcon icon={faDownload} />
+  </a>
+</div>
 
         <div
           className="flex flex-col items-center absolute top-[130px] right-[100px]"
